920ad1de7e767b53bcfabf9019346dc6ed47893f
How <chiteng2012@gmail.com>
2025-08-06 14:02:55 +0800

    fix: ensure App.tsx is a module for isolatedModules

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>PDF Combiner App</h1>
+      {/* Add your app content here */}
+    </div>
+  );
+};
+
+export default App;
+export {};
